2f8bf626daa3da9f03e36a058e267ccfa5d6102b
bigyan-sapkota <bigyansapkotavines@gmail.com>
2023-07-22 00:29:17 +0545

    completed my first responsive page :)

--- script.js
+++ script.js
@@ -2,9 +2,5 @@ const hamburger = document.getElementById("hamburger");
 const menu = document.getElementById("menu");
 
 hamburger.addEventListener("click", function() {
-  if (menu.classList.contains("invisible")) {
-    menu.classList.remove("invisible");
-  } else {
-    menu.classList.add("invisible");
-  }
+  menu.classList.toggle("invisible");
 });
