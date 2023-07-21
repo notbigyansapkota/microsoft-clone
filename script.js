2189876d300c73788014fccf6d74c1187c9a4e5a
bigyan-sapkota <bigyansapkotavines@gmail.com>
2023-07-22 00:20:57 +0545

    completed

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-const navHandler = () => {
-    
-}
+const hamburger = document.getElementById("hamburger");
+const menu = document.getElementById("menu");
+
+hamburger.addEventListener("click", function() {
+  if (menu.classList.contains("invisible")) {
+    menu.classList.remove("invisible");
+  } else {
+    menu.classList.add("invisible");
+  }
+});
